087326f25979f5af2877c2e87f2ee96e554e1d18
F1D022094_Rizky Insania Ramadhani <rizkyinsania26@gmail.com>
2025-05-16 02:29:30 +0800

    npm run build

--- src/scripts/pages/add-story/add-story-page.js
+++ src/scripts/pages/add-story/add-story-page.js
@@ -90,8 +90,8 @@ export default class AddStoryPage {
     });
 
     const defaultIcon = L.icon({
-      iconUrl: '../src/assets/images/marker-icon.png',
-      shadowUrl: '../src/assets/images/marker-shadow.png',
+      iconUrl: '../../../assets/images/marker-icon.png',
+      shadowUrl: '../../../assets/images/marker-shadow.png',
       iconSize: [25, 41],
       iconAnchor: [12, 41],
       popupAnchor: [1, -34],
